df708e8f0eb325e222e654907974f0472276e1e0
yungblud <killi8n@gmail.com>
2023-02-26 14:20:09 +0900

    Added user create method

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -1,7 +1,25 @@
+import { prisma } from "../instance"
+
 export default class User {
     public id!: number
     public createdAt!: Date
     public email!: string
     public password?: string
     public passwordSalt?: string
+
+    public async create() {
+        try {
+            const user = await prisma.user.create({
+                'data': {
+                    email: this.email,
+                    password: this.password,
+                    passwordSalt: this.passwordSalt
+                }
+            })
+            return user
+        } catch (e) {
+            console.error(e)
+            return null
+        }
+    }
 }
